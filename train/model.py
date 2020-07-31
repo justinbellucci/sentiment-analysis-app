34c89f8b6744c715bde5314e11aae3875ddd93cf
justinbellucci <justin.bellucci@me.com>
2020-07-31 13:33:46 -0600

    LSTM clssifier framework

---
+++ train/model.py
@@ -0,0 +1,17 @@
+# PROGRAMMER: Justin Bellucci 
+# DATE CREATED: 07_31_2020                                  
+# REVISED DATE: 
+
+import torch.nn as nn
+
+class LSTMClassifier(nn.Module):
+    """ LSTM based RNN to perform sentiment analysis
+    
+    """
+    def __init__(self, embedding_dim, hidden_dim, vocab_size):
+        """ Initialize the model
+        
+        """
+
+    def forward(self, x):
+        return None
