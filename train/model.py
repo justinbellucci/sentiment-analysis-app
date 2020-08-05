01afb1f9ede9d04502d2da7e3fd2e7fa14c8b0ca
justinbellucci <justin.bellucci@me.com>
2020-08-05 10:32:10 -0600

    LSTM classifier class with forward functionality

--- train/model.py
+++ train/model.py
@@ -8,10 +8,34 @@ class LSTMClassifier(nn.Module):
     """ LSTM based RNN to perform sentiment analysis
     
     """
-    def __init__(self, embedding_dim, hidden_dim, vocab_size):
-        """ Initialize the model
-        
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, n_layers=1, drop_prob=0.5):
+        """ Initialize the model by setting up the various 
+            layers.
         """
+        super(LSTMClassifier, self).__init__()
 
-    def forward(self, x):
-        return None
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=drop_prob)
+        self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(in_features=hidden_dim, out_features=1)
+        self.sigmoid = nn.Sigmoid()
+        self.word_dict = None
+
+    def forward(self, x, hidden):
+        """ Perform a forward pass of our model on some input and 
+            hidden state.
+        """
+        batch_size = x.size(0)
+        x = x.long()
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+        out = self.dropout(lstm_out)
+        out = self.fc(out)    
+        sig_out = self.sigmoid(out)
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1]
+
+        return sig_out, hidden
